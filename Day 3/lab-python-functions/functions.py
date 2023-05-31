4b68a3a3cdfb2f94156bf40f7e03154b700b9af8
AnaswaraVijayan <132006297+AnaswaraVijayan@users.noreply.github.com>
2023-05-31 21:53:07 +0200

    Lab_work

---
+++ Day 3/lab-python-functions/functions.py
@@ -0,0 +1,60 @@
+def get_unique_list_f(lst):
+    """
+    Takes a list as an argument and returns a new list with unique elements from the first list.
+
+    Parameters:
+    lst (list): The input list.
+
+    Returns:
+    list: A new list with unique elements from the input list.
+    """
+    unique=[]
+    for number in lst:
+        if number not in unique:
+            unique.append(number)
+           
+    return unique
+
+def count_case_f(string):
+    """
+    Returns the number of uppercase and lowercase letters in the given string.
+
+    Parameters:
+    string (str): The string to count uppercase and lowercase letters in.
+
+    Returns:
+    A tuple containing the count of uppercase and lowercase letters in the string.
+    """
+    uppercase=sum([i.isupper() for i in string ])
+    lowercase=sum([i.islower() for i in string ])
+    return uppercase,lowercase
+
+import string
+
+def remove_punctuation_f(sentence):
+    """
+    Removes all punctuation marks (commas, periods, exclamation marks, question marks) from a sentence.
+
+    Parameters:
+    sentence (str): A string representing a sentence.
+
+    Returns:
+    str: The sentence without any punctuation marks.
+    """
+    return sentence.replace(",","").replace(".","").replace("?","").replace(":","").replace("!","").replace(")","")
+
+def word_count_f(sentence):
+    """
+    Counts the number of words in a given sentence. To do this properly, first it removes punctuation from the sentence.
+    Note: A word is defined as a sequence of characters separated by spaces. We can assume that there will be no leading or trailing spaces in the input sentence.
+    
+    Parameters:
+    sentence (str): A string representing a sentence.
+
+    Returns:
+    int: The number of words in the sentence.
+    """
+    new_sentence=remove_punctuation(sentence)
+    new_list=len(new_sentence.split())
+    
+    return new_list
